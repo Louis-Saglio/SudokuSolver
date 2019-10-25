09850ef5c3f02c003d16d3b379c0e98ffa2058b6
Louis-Saglio <louis.saglio@sfr.fr>
2019-10-25 17:30:09 +0200

    Restart project but with the goal of solving with a genetic algorithm

--- main.py
+++ main.py
@@ -1,34 +1,69 @@
-import random
+from random import randint
+from time import time
+from typing import Tuple, Optional, Set
+
+from genetic import Individual
 
 
-class Grid:
-
-    def __init__(self, dimensions=(9, 9), pre_filled_nbr=0):
-        self.dimensions = dimensions
-        self.cells = {}
-        for position in self._get_random_positions(pre_filled_nbr):
-            self.cells[position] = 0
-
-    def _get_random_positions(self, nbr):
-        # todo : max nbr **3
-        # todo : forbid doubles
-        return tuple(zip(*[random.choices(range(dim_size), k=nbr) for dim_size in self.dimensions]))
+class Position:
+    def __init__(self, coordinates: Tuple[int, int]):
+        self.coordinates = coordinates
 
     def __repr__(self):
-        if len(self.dimensions) != 2:
-            return f"{self.__class__.__name__}{{dimensions={self.dimensions}, cells={self.cells}}}"
-        rep = []
-        for x in range(self.dimensions[0]):
-            for y in range(self.dimensions[1]):
-                rep.append(f"{self.cells.get((x, y), ' ')} ")
-            rep.append('\n')
-        return ''.join(rep)
+        return f"{self.coordinates[0]}, {self.coordinates[1]}"
 
 
-def main():
-    grid = Grid(pre_filled_nbr=30)
-    print(grid)
+class Cell:
+    def __init__(self, position: Position, value: Optional[int]):
+        self.position = position
+        self.value = value
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.position}, value={self.value})"
 
 
-if __name__ == '__main__':
-    main()
+class Grid(Individual):
+    def __init__(self, given_cells: Set[Cell]):
+        self.given_cells = given_cells
+        self.cells: Set[Cell] = given_cells.copy()
+        self.dimensions = (9, 9)
+        coordinates = {it.position.coordinates: it for it in self.given_cells}
+        for i in range(self.dimensions[0]):
+            for j in range(self.dimensions[1]):
+                if (i, j) not in coordinates:
+                    self.cells.add(Cell(Position((i, j)), None))
+        assert len(self.cells) == 81
+
+    def randomly_fill(self):
+        for cell in self.cells:
+            if cell not in self.given_cells:
+                cell.value = randint(1, 9)
+
+    def rate(self) -> int:
+        rows, columns, squares = {}, {}, {}
+        for cell in self.cells:
+            row_key = cell.position.coordinates[0]
+            column_key = cell.position.coordinates[1]
+            square_key = (cell.position.coordinates[0] % 3, cell.position.coordinates[1] % 3)
+            if row_key not in rows:
+                rows[row_key] = set()
+            rows[row_key].add(cell.value)
+            if column_key not in columns:
+                columns[column_key] = set()
+            columns[column_key].add(cell.value)
+            if square_key not in squares:
+                squares[square_key] = set()
+            squares[square_key].add(cell.value)
+        return (
+            sum([len(it) for it in rows.values()])
+            + sum([len(it) for it in columns.values()])
+            + sum([len(it) for it in squares.values()])
+        )
+
+
+if __name__ == "__main__":
+    start = time()
+    grid = Grid({Cell(Position((3, 5)), 8)})
+    grid.randomly_fill()
+    print(grid.rate())
+    print("\n".join([str(it) for it in grid.cells]))
